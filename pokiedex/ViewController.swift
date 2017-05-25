4fcdf574a19e2f966377734662ec625eb5280d89
MacBook Pro <macbookpro@macbooks-mbp>
2017-05-25 11:56:54 +0430

    Initial Commit

---
+++ pokiedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokiedex
+//
+//  Created by MacBook Pro on 1396/3/4 ه‍.ش..
+//  Copyright © 1396 ه‍.ش. mohammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pokiedexTests/pokiedexTests.swift
@@ -0,0 +1,36 @@
+//
+//  pokiedexTests.swift
+//  pokiedexTests
+//
+//  Created by MacBook Pro on 1396/3/4 ه‍.ش..
+//  Copyright © 1396 ه‍.ش. mohammad. All rights reserved.
+//
+
+import XCTest
+@testable import pokiedex
+
+class pokiedexTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
